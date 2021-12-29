6c05673ec61a9fcc9eb1b5b3e5957125281d1efd
Muhammad Naufal <46437079+naufalist@users.noreply.github.com>
2021-12-29 09:55:26 +0700

    initial commit

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQConsumer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+
+            // create connection
+            using var connection = factory.CreateConnection();
+
+            // create channel
+            using var channel = connection.CreateModel();
+
+            // declare queue with name "demo-queue"
+            channel.QueueDeclare(
+                "demo-queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            // create consumer
+            var consumer = new EventingBasicConsumer(channel);
+
+            // receive message
+            consumer.Received += (sender, e) =>
+            {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+
+            // consume
+            channel.BasicConsume("demo-queue", true, consumer);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQProducer/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQProducer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+
+            // create connection
+            using var connection = factory.CreateConnection();
+
+            // create channel
+            using var channel = connection.CreateModel();
+
+            // declare queue with name "demo-queue"
+            channel.QueueDeclare(
+                "demo-queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            // intialize message and serialize
+            var message = new { Name = "Producer", Message = "Hi! I'm a producer" };
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+            // publish body to (empty) default exchange rabbitMQ
+            // routingKey: demo-queue
+            channel.BasicPublish("", "demo-queue", null, body);
+        }
+    }
+}
