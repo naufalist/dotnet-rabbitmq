5a88d94c6a7ba7da1923458390bcff050a2b9ac9
Muhammad Naufal <contact.naufalist@gmail.com>
2022-08-27 13:21:12 +0700

    clean up

--- RabbitMQConsumer/TopicExchangeConsumer.cs
+++ RabbitMQConsumer/TopicExchangeConsumer.cs
@@ -12,12 +12,21 @@ namespace RabbitMQConsumer
     {
         public static void Consume(IModel channel)
         {
-            // declare exchange
-            channel.ExchangeDeclare("demo-topic-exchange", ExchangeType.Topic);
+            // declare exchange with name "demo-topic-exchange"
+            channel.ExchangeDeclare(
+                exchange: "demo-topic-exchange",
+                type: ExchangeType.Topic,
+                durable: true,
+                autoDelete: false,
+                arguments: new Dictionary<string, object>
+                {
+                    {"x-message-ttl", 30000 } // ttl for the message in miliseconds (30 secs)
+                }
+            );
 
             // declare queue with name "demo-topic-queue"
             channel.QueueDeclare(
-                "demo-topic-queue",
+                queue: "demo-topic-queue",
                 durable: true,
                 exclusive: false,
                 autoDelete: false,
@@ -25,10 +34,19 @@ namespace RabbitMQConsumer
             );
 
             // bind a queue to an exchange
-            channel.QueueBind("demo-topic-queue", "demo-topic-exchange", "account.*");
+            channel.QueueBind(
+                queue: "demo-topic-queue",
+                exchange: "demo-topic-exchange",
+                routingKey: "account.*",
+                arguments: null
+            );
 
             // this will make consumer to fetch 10 messages at the time
-            channel.BasicQos(0, 10, false);
+            channel.BasicQos(
+                prefetchSize: 0,
+                prefetchCount: 10,
+                global: false
+            );
 
             // declare consumer
             var consumer = new EventingBasicConsumer(channel);
@@ -36,11 +54,16 @@ namespace RabbitMQConsumer
             {
                 var body = e.Body.ToArray();
                 var message = Encoding.UTF8.GetString(body);
-                Console.WriteLine(message);
+                Console.WriteLine($"[{DateTime.Now}] Consumed: {message}");
             };
 
             // started consuming
-            channel.BasicConsume("demo-topic-queue", true, consumer);
+            channel.BasicConsume(
+                queue: "demo-topic-queue",
+                autoAck: true,
+                consumer: consumer
+            );
+
             Console.WriteLine("Consumer started");
             Console.ReadLine();
         }
--- RabbitMQProducer/TopicExchangeProducer.cs
+++ RabbitMQProducer/TopicExchangeProducer.cs
@@ -2,10 +2,8 @@
 using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace RabbitMQProducer
 {
@@ -13,14 +11,26 @@ namespace RabbitMQProducer
     {
         public static void Publish(IModel channel)
         {
-            // ttl for the message in miliseconds
-            var ttl = new Dictionary<string, object>
-            {
-                {"x-message-ttl", 30000 } // 30 secs
-            };
-
             // declare exchange with name "demo-topic-exchange"
-            channel.ExchangeDeclare("demo-topic-exchange", ExchangeType.Topic, arguments: ttl);
+            channel.ExchangeDeclare(
+                exchange: "demo-topic-exchange",
+                type: ExchangeType.Topic,
+                durable: true,
+                autoDelete: false,
+                arguments: new Dictionary<string, object>
+                {
+                    {"x-message-ttl", 30000 } // ttl for the message in miliseconds (30 secs)
+                }
+            );
+
+            // declare queue with name "demo-topic-queue"
+            channel.QueueDeclare(
+                queue: "demo-topic-queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
 
             // initialize count for multiple messages
             var count = 0;
@@ -28,12 +38,18 @@ namespace RabbitMQProducer
             while (true)
             {
                 // intialize message and serialize
-                var message = new { Name = "Producer", Message = $"Hi! I'm a producer. Count: {count}" };
+                var message = $"Balance: Rp 100000. Count: {count}";
                 var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
 
-                // publish body to demo-direct-exchange
-                // routingKey: account.init
-                channel.BasicPublish("demo-topic-exchange", "user.update", null, body);
+                // publish body to demo-topic-exchange
+                // routingKey: account.balance
+                channel.BasicPublish(
+                    exchange: "demo-topic-exchange",
+                    routingKey: "account.balance",
+                    basicProperties: null,
+                    body: body
+                );
+                Console.WriteLine($"[{DateTime.Now}] Published: {message}");
 
                 // increment
                 count++;
