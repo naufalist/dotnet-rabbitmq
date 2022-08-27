7cb1119dc42b19379e420bd7094198e15e99ff6d
Muhammad Naufal <contact.naufalist@gmail.com>
2022-08-27 13:35:16 +0700

    clean up

--- RabbitMQConsumer/FanoutExchangeConsumer.cs
+++ RabbitMQConsumer/FanoutExchangeConsumer.cs
@@ -12,12 +12,21 @@ namespace RabbitMQConsumer
     {
         public static void Consume(IModel channel)
         {
-            // declare exchange
-            channel.ExchangeDeclare("demo-fanout-exchange", ExchangeType.Fanout);
+            // declare exchange with name "demo-fanout-exchange"
+            channel.ExchangeDeclare(
+                exchange: "demo-fanout-exchange",
+                type: ExchangeType.Fanout,
+                durable: true,
+                autoDelete: false,
+                arguments: new Dictionary<string, object>
+                {
+                    {"x-message-ttl", 30000 } // ttl for the message in miliseconds (30 secs)
+                }
+            );
 
             // declare queue with name "demo-fanout-queue"
             channel.QueueDeclare(
-                "demo-fanout-queue",
+                queue: "demo-fanout-queue",
                 durable: true,
                 exclusive: false,
                 autoDelete: false,
@@ -25,10 +34,19 @@ namespace RabbitMQConsumer
             );
 
             // bind a queue to an exchange
-            channel.QueueBind("demo-fanout-queue", "demo-fanout-exchange", string.Empty);
+            channel.QueueBind(
+                queue: "demo-fanout-queue",
+                exchange: "demo-fanout-exchange",
+                routingKey: string.Empty,
+                arguments: null
+            );
 
             // this will make consumer to fetch 10 messages at the time
-            channel.BasicQos(0, 10, false);
+            channel.BasicQos(
+                prefetchSize: 0,
+                prefetchCount: 10,
+                global: false
+            );
 
             // declare consumer
             var consumer = new EventingBasicConsumer(channel);
@@ -36,11 +54,16 @@ namespace RabbitMQConsumer
             {
                 var body = e.Body.ToArray();
                 var message = Encoding.UTF8.GetString(body);
-                Console.WriteLine(message);
+                Console.WriteLine($"[{DateTime.Now}] Consumed: {message}");
             };
 
             // started consuming
-            channel.BasicConsume("demo-fanout-queue", true, consumer);
+            channel.BasicConsume(
+                queue: "demo-fanout-queue",
+                autoAck: true,
+                consumer: consumer
+            );
+
             Console.WriteLine("Consumer started");
             Console.ReadLine();
         }
--- RabbitMQProducer/FanoutExchangeProducer.cs
+++ RabbitMQProducer/FanoutExchangeProducer.cs
@@ -13,14 +13,26 @@ namespace RabbitMQProducer
     {
         public static void Publish(IModel channel)
         {
-            // ttl for the message in miliseconds
-            var ttl = new Dictionary<string, object>
-            {
-                {"x-message-ttl", 30000 } // 30 secs
-            };
-
             // declare exchange with name "demo-fanout-exchange"
-            channel.ExchangeDeclare("demo-fanout-exchange", ExchangeType.Fanout, arguments: ttl);
+            channel.ExchangeDeclare(
+                exchange: "demo-fanout-exchange",
+                type: ExchangeType.Fanout,
+                durable: true,
+                autoDelete: false,
+                arguments: new Dictionary<string, object>
+                {
+                    {"x-message-ttl", 30000 } // ttl for the message in miliseconds (30 secs)
+                }
+            );
+
+            // declare queue with name "demo-fanout-queue"
+            channel.QueueDeclare(
+                queue: "demo-fanout-queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
 
             // initialize count for multiple messages
             var count = 0;
@@ -28,19 +40,27 @@ namespace RabbitMQProducer
             while (true)
             {
                 // intialize message and serialize
-                var message = new { Name = "Producer", Message = $"Hi! I'm a producer. Count: {count}" };
+                var message = $"Balance: Rp 100000. Count: {count}";
                 var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
 
                 // set header props
                 // basic props not needed, just to show that it really doesn't matter
                 // what you pass in header
-                var properties = channel.CreateBasicProperties();
-                properties.Headers = new Dictionary<string, object> { { "account", "new" } };
+                var basicProperties = channel.CreateBasicProperties();
+                basicProperties.Headers = new Dictionary<string, object>
+                {
+                    { "account", "initialize" }
+                };
 
                 // publish body to demo-fanout-exchange
-                // routingKey: account.init
-                // property: properties
-                channel.BasicPublish("demo-fanout-exchange", "account.init", properties, body);
+                // routingKey: account.balance (routingKey not needed, just to show that it really doesn't matter
+                channel.BasicPublish(
+                    exchange: "demo-fanout-exchange",
+                    routingKey: "account.balance",
+                    basicProperties: basicProperties,
+                    body: body
+                );
+                Console.WriteLine($"[{DateTime.Now}] Published: {message}");
 
                 // increment
                 count++;
